09a4816d61af01de7db70e530b0362779d9d4fb7
lequilelorenz0 <lorenzolequile34@gmail.com>
2025-03-03 22:57:27 +0000

    update

--- TPSIT/es_a_op.sh
+++ TPSIT/es_a_op.sh
@@ -1,6 +1,29 @@
+
 #!/bin/bash
-echo -n "digita un numero:"
-read N1
-echo -n "digitane un altro:"
-read N2
-echo "il risultato della somma è: $((N1+N2))"
+
+# Controllo  3 argomenti
+if [ $# -ne 3 ]; then
+    echo "Uso: $0 <operazione> <n1> <n2>"
+    exit 1
+fi
+operz=$1
+N1=$2
+N2=$3
+
+# Eseguiamo l'operazione digitata
+if [ "$operazione" = "add" ]; then
+    echo "Risultato: $((N1 + N2))"
+elif [ "$operazione" = "sub" ]; then
+    echo "Risultato: $((N1 - N2))"
+elif [ "$operazione" = "mul" ]; then
+    echo "Risultato: $((N1 * N2))"
+elif [ "$operazione" = "div" ]; then
+    if [ "$ N2" -eq 0 ]; then
+        echo "Errore: divisione per zero!"
+        exit 2
+    fi
+    echo "Risultato: $((N1 / N2))"
+else
+    echo "Errore: operazione non supportata (usa add, sub, mul, div)"
+    exit 3
+fi
--- TPSIT/es_b_somma.sh
+++ TPSIT/es_b_somma.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Inizializza la variabiile
+i=1
+somma=0
+
+# Ciclo while per scorrere argomenti
+while [ $i -le $# ]; do
+    # Aggiungi l'argomento corrente alla somma
+    somma=$((somma + ${!i}))
+    i=$((i + 1))
+done
+
+# Stampa il risultato della somma
+echo "La somma degli argomenti è: $somma"
