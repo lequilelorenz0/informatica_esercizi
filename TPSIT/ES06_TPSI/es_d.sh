38467597913b52b9271f1a15084d1eb79cc4e731
lequilelorenz0 <lorenzolequile34@gmail.com>
2025-03-17 13:38:58 +0000

    upddate

--- TPSIT/ES06_TPSI/es_d.sh
+++ TPSIT/ES06_TPSI/es_d.sh
@@ -1,23 +1,37 @@
+/** ****************************************************************************************
+* \mainpage "operazioni matematiche in bash"
+* @brief Disegnare un triangolo rettangolo di asterischi con il numero di righe uguale all’argomento passato allo script. 
+Disegnare stampando degli asterischi un albero di natale
+
+* 
+* @author Lorenzo Lequile
+* @version 1.0 15/03/25 versione iniziale
+* @version 2.0 17/03/25 versione finale
+*/
 #!/bin/bash
 
 # Verifica che sia stato passato un argomento
 if [ "$1" = "" ]; then
-    echo "Uso: $0 <numero argomento passato>"
-    exit 1
+    echo "Uso: $0 <numero argomento passato>" # Messaggio di utilizzo corretto
+    exit 1 # Esce dallo script con codice di errore 1
 fi
 
+# Salva l'argomento passato  nella variabile numero_righe
 numero_righe=$1
 
 # Stampa del triangolo 
 i=$numero_righe
+# Ciclo while per il numero di righe
 while [ $i -gt 0 ]; do
     j=$i
+    # Ciclo interno per stampare gli asterischi nella riga corrente
     while [ $j -gt 0 ]; do
-        echo -n "*"
-        j=$((j - 1))
+        echo -n "*" # Stampa un asterisco 
+        j=$((j - 1)) # Decrementa il contatore interno
     done
-    echo
-    i=$((i - 1))
+    echo # Va a capo dopo aver stampato tutti gli asterischi della riga corrente
+    i=$((i - 1)) # Decrementa il contatore delle righe
 done
+
 
 
